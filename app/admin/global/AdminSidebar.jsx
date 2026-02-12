ab102afd89a34dcbb9b9a5f2cb21d013bccb921b
zk779 <59329882+zk779@users.noreply.github.com>
2026-02-12 18:31:12 +0500

    Update AdminSidebar.jsx

--- app/admin/global/AdminSidebar.jsx
+++ app/admin/global/AdminSidebar.jsx
@@ -13,6 +13,7 @@ import { useAdminAuth } from "@/app/context/AdminAuthContext";
 const PAGE_PERMISSIONS = {
     "/admin/orders": "order.read",
     "/admin/add-shop-product": "product.read",
+    "/admin/customers": "customers.read",
     "/admin/blog": "blog.read",
     "/admin/blog-management": "blog.read",
     "/admin/our-portfolio": "portfolio.read",
@@ -84,6 +85,11 @@ const AdminSidebar = ({ collapsed, setCollapsed, setMenuOpen }) => {
                 id: 3,
                 slug: "/admin/our-portfolio",
                 languages: { en: { title: "Portfolio" }, ar: { title: "معرض الأعمال" } },
+            },
+            {
+                id:11,
+                slug: "/admin/customers",
+                languages: { en: { title: "Customers" }, ar: { title: "العملاء" } },
             },
             {
                 id: 5,
