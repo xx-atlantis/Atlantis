3c2e55b0365baab46015493c59f5191076809fd4
Murtaza/Ahmed <ahmed.sxx369@gmail.com>
2026-02-24 17:08:02 +0300

    Checkout image fix

--- app/[locale]/checkout/page.js
+++ app/[locale]/checkout/page.js
@@ -8,6 +8,7 @@ import { CheckCircle2, CreditCard, Wallet, Sparkles, Landmark, Tag, X, SaudiRiya
 import Script from "next/script";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import Image from "next/image";
 
 // =====================================================================
 // SAUDI RIYAL SYMBOL COMPONENT
