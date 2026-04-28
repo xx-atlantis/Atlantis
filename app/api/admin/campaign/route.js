2d935157eef42aa4bf1133f5b6470a28db17dec8
Xmurtaza <murtaza.ghani91@gmail.com>
2026-04-28 05:02:23 +0300

    email marketing fix

--- app/api/admin/campaign/route.js
+++ app/api/admin/campaign/route.js
@@ -8,8 +8,12 @@ function makeTransporter() {
     port: process.env.SMTP_PORT,
     secure: process.env.SMTP_PORT === "465",
     auth: {
+      type: "LOGIN",
       user: process.env.SMTP_USER,
       pass: process.env.SMTP_PASS,
+    },
+    tls: {
+      rejectUnauthorized: false,
     },
   });
 }
